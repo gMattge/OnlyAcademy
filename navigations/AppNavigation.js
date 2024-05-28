9bd5c11db7a6b51d1d7f7d781b41dce00fd163b0
gMattge <gmattge@minha.fag.edu.br>
2024-05-28 20:43:07 -0300

    metodo de pagamento

--- navigations/AppNavigation.js
+++ navigations/AppNavigation.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import ProfileScreen from '../screens/Profile';
 import Camera from '../screens/Camera';
+import Pagamento from "../screens/pagamento";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -10,10 +12,10 @@ const AppNavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{ headerShown: false }} initialRouteName='Camera'>
+        screenOptions={{ headerShown: false }} initialRouteName='Pagamento'>
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="Camera" component={Camera} />
-
+        <Stack.Screen name="Pagamento" component={Pagamento} />
       </Stack.Navigator>
     </NavigationContainer>
   )
